07ebc069a4f2306a49f2ef307f57e6761b6f1bdc
luisalejandrovm <LuisAlejandroVM@hotmail.com>
2021-03-18 14:26:31 -0600

    first commit

---
+++ spambot_wha.py
@@ -0,0 +1,26 @@
+'''
+    Description:
+    Send messages from python to whatsapp.
+
+    Author: AlejandroV
+    Video: https://youtu.be/4DGBVlMmzYs
+'''
+import pyautogui, webbrowser
+from time import sleep
+
+# include your country code and no spaces
+phone = 'YOUR_PHONE_HERE'
+
+# open whatsapp web
+webbrowser.open(f'https://web.whatsapp.com/send?phone={phone}')
+sleep(10)
+
+# open a simple file
+with open('song.txt','r') as file:
+    # iterate
+    for line in file:
+        # write the lines on page opened
+        pyautogui.typewrite(line)
+
+        # press enter (send message)
+        pyautogui.press("enter")
